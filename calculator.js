f2dfb4add86da9bf63ca7993fd68493f48d2087e
Barandun <j.barandun@lincolninternational.ch>
2021-09-09 15:44:49 +0200

    BMI Calculator v1

--- calculator.js
+++ calculator.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/bmicalculator.html");
 });
 
 app.post("/", function(req, res) {
@@ -14,6 +14,14 @@ app.post("/", function(req, res) {
   var result = num1 + num2;
 
   res.send("The result of the calculation is " + result);
+});
+
+app.post("/bmicalculator", function(req, res) {
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height);
+  var bmi = weight / (height*height);
+
+  res.send("Your bmi is " + bmi);
 });
 
 app.listen(3000);
