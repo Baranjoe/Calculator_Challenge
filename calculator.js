3316c4f3b6cc68d68a4c0825172bafb3ab9a61d9
Barandun <j.barandun@lincolninternational.ch>
2021-09-09 13:56:20 +0200

    Small Change

--- calculator.js
+++ calculator.js
@@ -5,4 +5,8 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function(req, res) {
+  res.send("thanks for sending that!");
+});
+
 app.listen(3000);
